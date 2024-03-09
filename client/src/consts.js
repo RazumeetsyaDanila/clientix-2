3ed6d471b7b29b4b6a393e0067a3bb6ceefb0c8d
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2024-03-09 14:16:28 +0300

    ...

--- client/src/consts.js
+++ client/src/consts.js
@@ -1,4 +1,4 @@
-let placeOfWork = 'prod' //work - рабочий комп, home - домашний комп, prod - рабочий сервер
+let placeOfWork = 'home' //work - рабочий комп, home - домашний комп, prod - рабочий сервер
 
 export let REACT_APP_API_URL
 export let REACT_APP_URL
@@ -8,7 +8,7 @@ if(placeOfWork == 'work'){
     REACT_APP_URL = 'http://localhost:3000/'
 }
 if(placeOfWork == 'home'){
-    REACT_APP_API_URL = 'http://192.168.0.14:5000/'
+    REACT_APP_API_URL = 'http://192.168.0.14:5001/'
     REACT_APP_URL = 'http://localhost:3000/'
 }
 if(placeOfWork == 'prod'){
